7df222a65dcbbb0c86065743393fe2a51e98e2a9
jkanchelov <jkanchelov@gmail.com>
2019-12-01 19:27:46 +0200

    Remove prompt

--- src/prompt/activePrompt.ts
+++
@@ -1,5 +0,0 @@
-import * as prompts from "prompts";
-import CommandSender from "../models/commandSender";
-import WSHandler from "../models/wsClientsHandler";
-
-export default async (commandSender: CommandSender, wsHandler: WSHandler) => {};
